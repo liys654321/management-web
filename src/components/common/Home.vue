e9d289c16f23a67bdae45c932451c080d67f4112
liyongshun <963655459@qq.com>
2019-08-20 15:35:15 +0800

    调剂

---
+++ src/components/common/Home.vue
@@ -0,0 +1,97 @@
+<template>
+
+    <div class="main">
+
+
+        <el-container>
+            <el-header>系统管理</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <el-row class="tac">
+                        <el-col :span="12">
+                            <h5>默认颜色</h5>
+                            <el-menu
+                                    default-active="2"
+                                    class="el-menu-vertical-demo"
+                                    @open="handleOpen"
+                                    @close="handleClose">
+                                <el-submenu index="1">
+                                    <template slot="title">
+                                        <i class="el-icon-location"></i>
+                                        <span>导航一</span>
+                                    </template>
+                                    <el-menu-item-group>
+                                        <template slot="title">分组一</template>
+                                        <el-menu-item index="1-1">选项1</el-menu-item>
+                                        <el-menu-item index="1-2">选项2</el-menu-item>
+                                    </el-menu-item-group>
+                                    <el-menu-item-group title="分组2">
+                                        <el-menu-item index="1-3">选项3</el-menu-item>
+                                    </el-menu-item-group>
+                                    <el-submenu index="1-4">
+                                        <template slot="title">选项4</template>
+                                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                                    </el-submenu>
+                                </el-submenu>
+                            </el-menu>
+                        </el-col>
+                    </el-row>
+                </el-aside>
+                <el-container>
+                    <el-main></el-main>
+                </el-container>
+            </el-container>
+            <el-footer></el-footer>
+        </el-container>
+
+    </div>
+
+</template>
+<script>
+
+		export default {
+				data() {
+
+				},
+                methods: {
+                        handleOpen(key, keyPath) {
+                                console.log(key, keyPath);
+                        },
+                        handleClose(key, keyPath) {
+                                console.log(key, keyPath);
+                        }
+                }
+		};
+</script>
+<style>
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+
+    .el-aside {
+       /* background-color: #D3DCE6;
+        color: #333;*/
+        text-align: center;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        height: 100%;
+    }
+    .main {
+        width: 100%;
+        height: 100%;
+    }
+
+    .el-container {
+        height: 100%;
+    }
+
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,55 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios';
+import qs from "qs";
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(Element,{
+		size: 'small'
+});
+Vue.prototype.$axios = axios;
+Vue.prototype.$qs = qs;
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+		router,
+		render: c => c(App),
+		created: function () {
+				/*let params = '{"base":{"type":1,"flumeName":"6","flumeGrade":"6","materialType":1,"location":"6"},"param":{"type":1,"normalFlux":"6","flumeBodyType":"6"}}';
+				var par = new FormData();
+				par.append("params",params);
+				let config = {
+						headers: {
+								'Content-Type': 'application/x-www-form-urlencoded'
+						}
+				}
+				this.$axios.get("/api/structure/save?params=" + this.$qs.stringify(par),config).then((res) => {
+						console.log(res);
+				})*/
+				/*let params = '{"base":{"type":1,"flumeName":"6","flumeGrade":"6","materialType":1,"location":"6"},"param":{"type":1,"normalFlux":"6","flumeBodyType":"6"}}';
+				this.$axios({
+						url: '/api/structure/save',  //请求路径（接口）
+						method: 'POST', //请求方式
+						headers: { 'content-type': 'application/x-www-form-urlencoded' }, // 请求头，发送FormData格式的数据，必须是 这种请求头。
+						data: qs.stringify({params: params}),
+				})*/
+		}
+		/*method: {
+	
+			created () {
+			console.log('==============' + 'created' + '===================')
+			console.log(this.$el)
+			console.log(this.$data)
+			console.log(this.filter)
+			},
+	
+			Mounted() {
+			$axios.get("www.baidu.com").then((res) => {
+				console.log(res);
+			})
+			}
+		}*/
+}).$mount("#app")
